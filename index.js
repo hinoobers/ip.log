58f9750e123c59cd6fd3605aa8a9cec46ca0b0ae
hinoob <redisolenmina1@gmail.com>
2026-01-05 02:27:54 +0200

    Lihtsad ruuterid

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+app.get("/checkip", (req, res) => {
+
+});
+
+app.post("/login", (req, res) => {
+
+});
+
+app.post("/register", (req, res) => {
+
+});
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
