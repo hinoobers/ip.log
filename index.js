baeaa9e946b558995a5a8c1eb8efc637f54863bc
hinoob <redisolenmina1@gmail.com>
2026-01-05 02:39:33 +0200

    Andmebaasi testimine

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const app = express();
 
+const sqlDatabase = require("./util/mysql");
+
+app.get("/", async (req, res) => {
+    // Andmebaasi test
+    try {
+        const [rows, fields] = await sqlDatabase.query("SELECT 1 + 1 AS solution");
+        res.send(`The solution is: ${rows[0].solution}`);
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+});
+
 app.get("/checkip", (req, res) => {
 
 });
